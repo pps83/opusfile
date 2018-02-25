0f4f86c9c5bf970139c854df8e2e843208b30b7a
Pavel P <pavlov.pavel@gmail.com>
2018-02-25 13:18:49 -0800

    Single source compilation

---
+++ libopusfile.c
@@ -0,0 +1,8 @@
+#ifdef _MSC_VER
+#pragma warning(disable: 4244) // conversion from 'int' to 'opus_int16', possible loss of data
+#endif
+
+#include "../src/info.c"
+#include "../src/internal.c"
+#include "../src/opusfile.c"
+#include "../src/stream.c"
