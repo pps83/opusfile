23474588c4c141d0776e1d087d0ba6c96df49d3f
Pavel P <pavlov.pavel@gmail.com>
2018-03-02 09:25:51 -0800

    Android compilation fixed (unified headers and 64-bit stdio)

--- libopusfile.c
+++ libopusfile.c
@@ -2,6 +2,11 @@
 #pragma warning(disable: 4244) // conversion from 'int' to 'opus_int16', possible loss of data
 #endif
 
+#ifdef __ANDROID__ // avoid issues with 64-bit stdio
+#include <stdio.h>
+#include <unistd.h>
+#endif
+
 #include "../src/info.c"
 #include "../src/internal.c"
 #include "../src/opusfile.c"
